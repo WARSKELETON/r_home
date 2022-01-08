11571aaa3a03574955ee803e3e02f8dbff835a66
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2022-01-08 00:48:10 +0000

    My Local Activities Repository

--- lib/domain/my_local_activities/i_my_local_activities_repository.dart
+++ lib/domain/my_local_activities/i_my_local_activities_repository.dart
@@ -2,6 +2,7 @@ import 'package:r_home/domain/local_activities/local_activity.dart';
 
 abstract class IMyLocalActivitiesRepository {
   Stream<List<LocalActivity>> watchAll();
+  Stream<LocalActivity> watch(String localActivityUuid);
   Future<void> create(LocalActivity localActivity);
   Future<void> update(LocalActivity localActivity, String docId);
   void delete(LocalActivity localActivity);
--- lib/infrastructure/my_local_activities/my_local_activities_repository.dart
+++ lib/infrastructure/my_local_activities/my_local_activities_repository.dart
@@ -7,6 +7,7 @@ import 'package:r_home/infrastructure/my_local_activities/my_local_activities_ex
 
 class MyLocalActivitiesRepository implements IMyLocalActivitiesRepository {
   final FirebaseFirestore _firestore;
+  static const String PARENT_COLLECTION = "my-activities";
   static const String ACTIVITIES_COLLECTION = "my-activities";
   final IAuthFacade _authFacade;
 
@@ -17,13 +18,24 @@ class MyLocalActivitiesRepository implements IMyLocalActivitiesRepository {
     final userId = _authFacade.getSignedInUserId()!;
 
     final colRef = _firestore
-    .collection("my-homes")
-    .doc(userId)
-    .collection(ACTIVITIES_COLLECTION);
+        .collection(PARENT_COLLECTION)
+        .doc(userId)
+        .collection(ACTIVITIES_COLLECTION);
 
-    yield* colRef
-        .snapshots()
-        .map((query) => query.toListLocalActivity());
+    yield* colRef.snapshots().map((query) => query.toListLocalActivity());
+  }
+
+  @override
+  Stream<LocalActivity> watch(String localActivityUuid) async* {
+    final userId = _authFacade.getSignedInUserId()!;
+
+    final docRef = _firestore
+        .collection(PARENT_COLLECTION)
+        .doc(userId)
+        .collection(ACTIVITIES_COLLECTION)
+        .doc(localActivityUuid);
+
+    yield* docRef.snapshots().map((doc) => doc.toLocalActivity());
   }
 
   @override
@@ -31,18 +43,22 @@ class MyLocalActivitiesRepository implements IMyLocalActivitiesRepository {
     final userId = _authFacade.getSignedInUserId()!;
 
     _firestore
-      .collection("my-homes")
+      .collection(PARENT_COLLECTION)
       .doc(userId)
       .collection(ACTIVITIES_COLLECTION)
-      .doc(localActivity.name)
-      .set(LocalActivityDto.fromDomain(localActivity, userId).toJson())
+      .doc(localActivity.uuid)
+      .set(LocalActivityDto.fromDomain(localActivity).toJson())
       .then((_) => print("Activity created successfuly"))
       .catchError((onError) => print(onError));
   }
 
   @override
-  void delete(LocalActivity home) {}
+  void delete(LocalActivity home) {
+
+  }
 
   @override
-  Future<void> update(LocalActivity home, String docId) async {}
+  Future<void> update(LocalActivity home, String docId) async {
+    
+  }
 }
