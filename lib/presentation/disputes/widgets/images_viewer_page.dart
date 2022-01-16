d52caa9f3fd098abcb9841fb902545a06a930d62
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2022-01-16 23:18:06 +0000

    Fix import from removed package

--- lib/presentation/disputes/widgets/images_viewer_page.dart
+++ lib/presentation/disputes/widgets/images_viewer_page.dart
@@ -1,8 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
-import 'package:photo_view/photo_view_gallery.dart';
-import 'package:r_home/presentation/core/r_home_color_scheme.dart';
 
 class ImagesViewerPage extends StatelessWidget {
   final List<String> images;
