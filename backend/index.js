b98275d6666dfd026e34883a2254ffe7b744801d
Lucas Vicente <yugi.man22@gmail.com>
2022-01-18 20:30:01 +0000

    Backend vote rewards

--- backend/index.js
+++ backend/index.js
@@ -24,20 +24,72 @@ const observer = query.onSnapshot(querySnapshot => {
             const updatedDocRef = db.collection('disputes').doc(doc.uuid);
             const updatedDoc = await updatedDocRef.get();
             const data = updatedDoc.data();
-
-            if (data.votesInFavour > data.votesIrrelevant && data.votesInFavour > data.votesAgainst) {
-                const uuid = uuidv1();
-                const transactionDocRef = db.collection('user-data').doc(data.issuerUuid).collection('transactions').doc(uuid);
-                await transactionDocRef.set({
-                    amount: data.initialStake + data.initialStake * 0.5,
+            const usersVotedAgainst = data.usersVotedAgainst;
+            const usersVotedIrrelevent = data.usersVotedIrrelevent;
+            const usersVotedInFavour = data.usersVotedInFavour;
+            const initialStake = data.initialStake; 
+            
+            if (usersVotedInFavour.length > usersVotedIrrelevent.length && usersVotedInFavour.length > usersVotedAgainst.length) {
+                const uuidIssuer = uuidv1();
+                const transactionIssuerDocRef = db.collection('user-data').doc(data.issuerUuid).collection('transactions').doc(uuidIssuer);
+                await transactionIssuerDocRef.set({
+                    amount: initialStake + ((usersVotedIrrelevent.length * 10 + usersVotedAgainst.length * 10) / (usersVotedInFavour.length + 1)),
                     paymentMethod: "token",
                     receiverId: data.issuerUuid,
                     receiverUsername: data.issuerUsername,
                     senderId: "",
                     senderUsername: "",
                     type: "close_dispute",
-                    uuid: uuid,
+                    uuid: uuidIssuer,
                     ts: Timestamp.fromDate(new Date())
+                });
+
+                usersVotedInFavour.forEach(user => {
+                    const uuid = uuidv1();
+                    const transactionDocRef = db.collection('user-data').doc(user).collection('transactions').doc(uuid);
+                    await transactionDocRef.set({
+                        amount: 10 + ((usersVotedIrrelevent.length * 10 + usersVotedAgainst.length * 10) / (usersVotedInFavour.length + 1)),
+                        paymentMethod: "token",
+                        receiverId: user,
+                        receiverUsername: "",
+                        senderId: "",
+                        senderUsername: "",
+                        type: "close_dispute",
+                        uuid: uuid,
+                        ts: Timestamp.fromDate(new Date())
+                    });
+                });
+            } else if (usersVotedIrrelevent.length > usersVotedInFavour.length && usersVotedIrrelevent.length > usersVotedAgainst.length) {
+                usersVotedIrrelevent.forEach(user => {
+                    const uuid = uuidv1();
+                    const transactionDocRef = db.collection('user-data').doc(user).collection('transactions').doc(uuid);
+                    await transactionDocRef.set({
+                        amount: 10 + ((initialStake + usersVotedInFavour.length * 10 + usersVotedAgainst.length * 10) / usersVotedIrrelevent.length),
+                        paymentMethod: "token",
+                        receiverId: user,
+                        receiverUsername: "",
+                        senderId: "",
+                        senderUsername: "",
+                        type: "close_dispute",
+                        uuid: uuid,
+                        ts: Timestamp.fromDate(new Date())
+                    });
+                });
+            } else {
+                usersVotedAgainst.forEach(user => {
+                    const uuid = uuidv1();
+                    const transactionDocRef = db.collection('user-data').doc(user).collection('transactions').doc(uuid);
+                    await transactionDocRef.set({
+                        amount: 10 + ((initialStake + usersVotedInFavour.length * 10 + usersVotedIrrelevent.length * 10) / usersVotedAgainst.length),
+                        paymentMethod: "token",
+                        receiverId: user,
+                        receiverUsername: "",
+                        senderId: "",
+                        senderUsername: "",
+                        type: "close_dispute",
+                        uuid: uuid,
+                        ts: Timestamp.fromDate(new Date())
+                    });
                 });
             }
 
