5dd0f6221ed2e208b9e35eeebe4f1baf7cf60bde
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2022-01-19 16:49:57 +0000

    Fix home screens smaller sizes

--- lib/presentation/home/widgets/home_welcome_widget.dart
+++ lib/presentation/home/widgets/home_welcome_widget.dart
@@ -14,79 +14,87 @@ class HomeWelcomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image.asset(
-              imageAsset,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: 350.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Colors.white,
-              gradient: LinearGradient(
-                begin: FractionalOffset.topCenter,
-                end: FractionalOffset.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.6),
-                  Colors.transparent,
-                ],
-              )
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, top: 15, right: 16, bottom: 12),
-            child: Text(
-              title,
-              style: const TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.w600
-              ),
-            ),
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                transform: Matrix4.translation(vector.Vector3(0, 25, 0)),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        side: BorderSide(
-                          color: Theme.of(context).colorScheme.primaryBlue,
-                          width: 2
-                        )
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.75,
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 840 / MediaQuery.of(context).size.height * 0.7,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: AspectRatio(
+                      aspectRatio: 840 / MediaQuery.of(context).size.height * 0.7,
+                      child: Image.asset(
+                        imageAsset,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    overlayColor: MaterialStateProperty.resolveWith((states) {
-                        return states.contains(MaterialState.pressed)
-                            ? Theme.of(context).colorScheme.primaryBlue.withOpacity(0.2)
-                            : null;
-                    }),
-                    fixedSize: MaterialStateProperty.all<Size>(const Size(160, 45))
                   ),
-                  child: Text(
-                    buttonText,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primaryBlue,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17
+                  Container(
+                    height: 350.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.white,
+                      gradient: LinearGradient(
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter,
+                        colors: [
+                          Colors.black.withOpacity(0.6),
+                          Colors.transparent,
+                        ],
+                      )
                     ),
                   ),
-                  onPressed: onPressed
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, top: 15, right: 16, bottom: 12),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                ]
               ),
             ),
-          )
-        ]
+            Container(
+              transform: Matrix4.translation(vector.Vector3(0, -25, 0)),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      side: BorderSide(
+                        color: Theme.of(context).colorScheme.primaryBlue,
+                        width: 2
+                      )
+                    ),
+                  ),
+                  overlayColor: MaterialStateProperty.resolveWith((states) {
+                      return states.contains(MaterialState.pressed)
+                          ? Theme.of(context).colorScheme.primaryBlue.withOpacity(0.2)
+                          : null;
+                  }),
+                  fixedSize: MaterialStateProperty.all<Size>(const Size(160, 45))
+                ),
+                child: Text(
+                  buttonText,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primaryBlue,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17
+                  ),
+                ),
+                onPressed: onPressed
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
